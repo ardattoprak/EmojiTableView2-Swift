38ef6b3cce3a8dd6ee2c5beebeeb29a5b017608b
ardatoprak <ardattoprak@gmail.com>
2023-01-08 18:23:48 +0300

    Initial commit

---
+++ Emoji/Emoji.swift
@@ -0,0 +1,15 @@
+//
+//  Emoji.swift
+//  Emoji
+//
+//  Created by Arda Toprak on 8.01.2023.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
